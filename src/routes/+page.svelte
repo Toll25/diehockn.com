ae37c05c0ce2e7830cad5c4f0c0798ab79f52cd1
Toll25 <116108222+Toll25@users.noreply.github.com>
2024-12-15 14:09:35 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,5 @@
 <h1>Modified with github</h1>
 <h1>Another test change</h1>
 <h1>Yet Another test change</h1>
+<h1>Benamin</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
