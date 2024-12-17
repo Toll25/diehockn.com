5a2b8d862bccaba27999877034aa19000cd0259e
Toll <toll25@proton.me>
2024-12-17 21:13:46 +0100

    added tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,22 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		extend: {
+			colors: {
+				background: '#161616',
+				text: '#ffffff',
+				subtext: '#cccccc',
+				surface1: '#090909',
+				surface2: '#050505',
+				primary: '#8586e8',
+				secondary: '#81187c',
+				accent: '#d72d96'
+			}
+		}
+	},
 
-  plugins: []
+	plugins: []
 } satisfies Config;
