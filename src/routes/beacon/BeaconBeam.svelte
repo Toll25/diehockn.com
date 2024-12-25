bfd5f267d0edf053cdc900978b816f1ea94feec8
Toll <toll25@proton.me>
2024-12-25 23:45:05 +0100

    fixed sizing in beacon rows

--- src/routes/beacon/BeaconBeam.svelte
+++ src/routes/beacon/BeaconBeam.svelte
@@ -7,9 +7,9 @@
 	let elapsedSeconds = $derived((Date.now() - startTime) / 1000);
 </script>
 
-<div class="flex w-12 items-center justify-center">
+<div class="flex h-full w-12 items-center justify-center">
 	<div
-		class="h-8 w-8 animate-scroll bg-blend-multiply"
+		class="h-full w-8 animate-scroll bg-blend-multiply"
 		style="background-color: rgb({color[0]}, {color[1]}, {color[2]}); background-image: url('/BeaconAssets/beacon_beam_large.png');
        			animation-delay: calc(-1s * {elapsedSeconds}); "
 	></div>
--- src/routes/beacon/ColorRow.svelte
+++ src/routes/beacon/ColorRow.svelte
@@ -42,14 +42,14 @@
 	let { color, handleDelete }: { color: number[]; handleDelete: any } = $props();
 </script>
 
-<div class="flex flex-row font-mono" id="color">
+<div class="flex h-8 flex-row font-mono" id="color">
 	<BeaconBeam {color} />
 	<div class="mx-2 w-36 content-center">rgb({color[0]},{color[1]},{color[2]})</div>
 	<div class="mx-2 w-36 content-center">
 		hsl({rgbToHsl(color[0], color[1], color[2]).join(',')})
 	</div>
 	<div class="mx-2 content-center">{rgbToHex(color[0], color[1], color[2])}</div>
-	<div class="mx-2 flex h-8 w-8 cursor-pointer items-center justify-center">
+	<div class="mx-2 flex w-8 cursor-pointer items-center justify-center">
 		<Icon icon="material-symbols:delete" class="h-6 w-6" onclick={handleDelete} />
 	</div>
 </div>
