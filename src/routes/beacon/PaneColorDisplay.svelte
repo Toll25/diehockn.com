cb6bb71b217c23f8ea76f5351122c8cc42e5c0e8
Toll <toll25@proton.me>
2024-12-27 12:32:51 +0100

    centered images in used colors display

--- src/routes/beacon/PaneColorDisplay.svelte
+++ src/routes/beacon/PaneColorDisplay.svelte
@@ -2,7 +2,7 @@
 	let { color }: { color: string } = $props();
 </script>
 
-<div class="flex flex-row items-start">
+<div class="flex flex-row items-center">
 	<div
 		class="mr-1 h-4 w-4"
 		style="background-image: url('/BeaconAssets/Glass/{color}_stained_glass.png')"
