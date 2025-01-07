c30ac587631e614fe8639207f7c3d1ab1d0535b3
Toll <toll25@proton.me>
2025-01-07 23:17:06 +0100

    added MonkeyInABox and light effects

--- src/routes/plinko/Scene.svelte
+++ src/routes/plinko/Scene.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	import { T, useLoader } from '@threlte/core';
+	import { T, useTask } from '@threlte/core';
 	import { Environment, OrbitControls } from '@threlte/extras';
 
 	import Plane from './Plane.svelte';
-	import { AutoColliders, CollisionGroups, RigidBody, World } from '@threlte/rapier';
-	import CoinPusher2 from './CoinPusher2.svelte';
+	import { CollisionGroups, World } from '@threlte/rapier';
+	import CoinPusher from './CoinPusher.svelte';
+	import Emitter from './Emitter.svelte';
+	import MonkeyInABox from './MonkeyInABox.svelte';
 
 	let autoRotate: boolean = false;
 	let enableDamping: boolean = true;
@@ -14,6 +16,18 @@
 	let minPolarAngle: number = 0;
 	let maxPolarAngle: number = Math.PI;
 	let enableZoom: boolean = true;
+
+	let direction = 1;
+	let speed = 1;
+	let counter = 0.5;
+
+	const { start, stop, started, task } = useTask((delta) => {
+		const deltaSpeed = speed * delta;
+		counter += deltaSpeed * direction;
+		if (counter <= -1 || counter > 1) {
+			direction *= -1; // Reverse direction
+		}
+	});
 </script>
 
 <Environment url={'/billiard_hall_2k.hdr'} isBackground={true} />
@@ -30,20 +44,19 @@
 	/>
 </T.PerspectiveCamera>
 
-<T.DirectionalLight position={[5, 5, 5]} castShadow intensity={20} color={0xff0000} />
-<!-- <T.AmbientLight intensity={0.3} /> -->
+<T.DirectionalLight
+	position={[5, 5, counter * 5]}
+	castShadow
+	intensity={20}
+	color={((255 * Math.abs(counter)) << 16) + (0 << 8) + 255}
+/>
 
 <World>
 	<CollisionGroups groups={[0, 15]}>
 		<Plane />
 	</CollisionGroups>
-	<RigidBody>
-		<AutoColliders shape={'cuboid'}>
-			<T.Mesh receiveShadow castShadow rotation={[-Math.PI / 2, 0, 0]} position={[-1.18, 2.5, 0]}>
-				<T.BoxGeometry args={[0.2, 0.2, 0.2]} />
-				<T.MeshStandardMaterial color={'white'} />
-			</T.Mesh>
-		</AutoColliders>
-	</RigidBody>
-	<CoinPusher2 />
+	<Emitter />
+
+	<MonkeyInABox />
+	<CoinPusher />
 </World>
