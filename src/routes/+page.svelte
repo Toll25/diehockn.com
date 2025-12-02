525866806a4e49ccbc90305527b5c32b912bbe5e
Toll <toll25@proton.me>
2025-12-02 21:01:55 +0100

    reverse test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
 		<p class="transform text-9xl font-extrabold transition-all hover:drop-shadow-glow">
 			Des is die <em>Hockn</em>
 		</p>
-		<p class="text-lg font-light text-subtext">- Gerald Stoll, 2026</p>
+		<p class="text-lg font-light text-subtext">- Gerald Stoll, 2025</p>
 	</div>
 </div>
